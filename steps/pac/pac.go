1a0c26a333b0d4eb7ac73f6340b9a76e5cf93884
ldaruri <110803535+ldaruri@users.noreply.github.com>
2024-03-06 12:16:15 +0530

    Update pac.go

--- steps/pac/pac.go
+++ steps/pac/pac.go
@@ -6,18 +6,7 @@ import (
 	"github.com/openshift-pipelines/release-tests/pkg/store"
 )
 
-var _ = gauge.Step("Set <enable> section under <pipelinesAsCode> to <enable: true|false>", func(section, inputField, isEnable string) {
+var _ = gauge.Step("Set <auto-configure-new-github-repo> section under <pipelinesAsCode> to <auto-configure-new-github-repo: true|false>", func(section, inputField, isEnable string) {
 	pac.VerifyPipelinesAsCodeEnable(store.Clients(), section, inputField, isEnable)
 })
 
-var _ = gauge.Step("Verify the installersets related to PAC are <expectedStatus>", func(expectedStatus string) {
-	pac.VerifyInstallerSets(store.Clients(), expectedStatus)
-})
-
-var _ = gauge.Step("Verify that the pods related to PAC are <present|not present> from <namespace> namespace", func(expectedStatus, namespace string) {
-	pac.VerifyPACPodsStatus(store.Clients(), expectedStatus, namespace)
-})
-
-var _ = gauge.Step("Verify that the custom resource <pipelines-as-code> of type <pac> is <present|not present>", func(resource, resourceType, expectedStatus string) {
-	pac.VerifyPACCustomResource(store.Clients(), expectedStatus)
-})
